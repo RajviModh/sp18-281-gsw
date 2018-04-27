ba05c77ccabde16a2493a8cc84e71c5fd3c1e248
shahkevaln <keval.shah@sjsu.edu>
2018-04-26 20:44:07 -0700

    Login API for Starbucks App

--- Starbucks-master-3/Bolierplate/backend/main.go
+++ Starbucks-master-3/Bolierplate/backend/main.go
@@ -184,6 +184,49 @@ func (sp SignUpController) signup(w http.ResponseWriter, r *http.Request) {
 }
 
 
+type LoginController struct {
+	session *mgo.Session
+}
+
+func NewLoginController(mgoSession *mgo.Session) *LoginController {
+	return &LoginController{mgoSession}
+}
+
+func (lc LoginController) login(w http.ResponseWriter, r *http.Request) {
+
+	session, _ := store.Get(r, "cookie-name")
+
+	uname := r.FormValue("email")
+	pass := r.FormValue("password")
+
+	fmt.Println(uname)
+	fmt.Println(pass)
+
+	result := User{}
+	err := lc.session.DB("cmpe281").C("User").Find(bson.M{"username": uname}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if pass == result.Password {
+		fmt.Println("Success")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+
+		session.Values["authenticated"] = true
+		session.Save(r, w)
+		setSession(uname, w)
+
+
+	} else {
+		w.WriteHeader(202)
+	}
+
+
+
+}
+
 
 
 
