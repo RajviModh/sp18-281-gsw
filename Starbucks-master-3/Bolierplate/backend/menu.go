d0ee272303bbc9bf1c9b83aa63ea45fea2cb8dd2
rohanath <rohan.athavale@sjsu.edu>
2018-04-27 08:19:49 -0700

    Changes to Menu API

--- Starbucks-master-3/Bolierplate/backend/menu.go
+++ Starbucks-master-3/Bolierplate/backend/menu.go
@@ -47,3 +47,42 @@ func (oc OrderController) GetOrders(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(&options)
 }
+
+// Get Item Informationx
+
+/*func (oc OrderController) GetItemInfo(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var uuid string = params["item"]
+
+	fmt.Println(uuid)
+	result := Description{}
+	if err := oc.session.DB("test").C("ItemDesc").Find(bson.M{"Name": uuid}).One(&result); err != nil {
+			w.WriteHeader(404)
+			data := `{"status":"error","message":"Order not found"}`
+			json.NewEncoder(w).Encode(data)
+			return
+		}
+
+		// Write content-type, statuscode, payload
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
+
+		w.WriteHeader(200)
+		json.NewEncoder(w).Encode(result)
+}*/
+
+
+func main() {
+
+	r := mux.NewRouter()
+
+	// Get a UserController instance
+	oc := NewOrderController(getSession())
+	r.Methods("OPTIONS").HandlerFunc(IgnoreOption)
+
+	r.HandleFunc("/starbucks/getMenu", oc.GetOrders).Methods("GET")
+	//r.HandleFunc("/starbucks/addToCart", oc.AddToCart).Methods("POST")
+	fmt.Println("serving on port" + GetPort())
+	http.ListenAndServe(GetPort(), r)
+
+}
